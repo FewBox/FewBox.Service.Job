90fc85417c2b2e0333c6f204bbbeed7573872785
landpy <landpy@outlook.com>
2023-05-08 14:11:51 +0800

    Refine code.

--- FewBox.Service.Job/Startup.cs
+++ FewBox.Service.Job/Startup.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using FewBox.Core.Utility.Net;
 
 namespace FewBox.Service.Job
 {
@@ -19,6 +20,8 @@ namespace FewBox.Service.Job
 
         public void ConfigureServices(IServiceCollection services)
         {
+            NetworkUtility.IsCertificateNeedValidate = false;
+            NetworkUtility.IsEnsureSuccessStatusCode = false;
             var endpointEvents = Configuration.GetSection("EndpointEvents").Get<IList<EndpointEvent>>();
             services.AddSingleton(endpointEvents);
             services.AddLogging(configure =>
