5b2ecc77ff7786f471eea77040c76fb8546e7321
XL <xl@fewbox.com>
2023-10-22 18:38:27 +0800

    Refine code.

--- FewBox.Service.Job/EndpointEvent.cs
+++ FewBox.Service.Job/EndpointEvent.cs
@@ -2,6 +2,7 @@ namespace FewBox.Service.Job
 {
     public class EndpointEvent
     {
+        public string JWTToken { get; set; }
         public Endpoint Endpoint { get; set; }
         public Event Event { get; set; }
     }
--- FewBox.Service.Job/Job.cs
+++ FewBox.Service.Job/Job.cs
@@ -27,39 +27,79 @@ namespace FewBox.Service.Job
                     this.Logger.LogDebug(url);
                     object response;
                     dynamic body = JsonUtility.Deserialize<object>(endpointEvent.Event.Body);
-                    switch (endpointEvent.Event.EnumType)
+                    if (endpointEvent.JWTToken != null && endpointEvent.JWTToken.Length > 0)
                     {
-                        case EventType.Get:
-                            response = RestfulUtility.Get<object>(url, new List<Header> { });
-                            break;
-                        case EventType.Post:
-                            response = RestfulUtility.Post<object, object>(url, new Package<dynamic>
-                            {
-                                Headers = new List<Header> { },
-                                Body = body
-                            });
-                            break;
-                        case EventType.Put:
-                            response = RestfulUtility.Put<object, object>(url, new Package<dynamic>
-                            {
-                                Headers = new List<Header> { },
-                                Body = body
-                            });
-                            break;
-                        case EventType.Patch:
-                            response = RestfulUtility.Patch<object, object>(url, new Package<dynamic>
-                            {
-                                Headers = new List<Header> { },
-                                Body = body
-                            },
-                            "application/json-patch+json");
-                            break;
-                        case EventType.Delete:
-                            response = RestfulUtility.Delete<object>(url, new List<Header> { });
-                            break;
-                        default:
-                            response = new Object { };
-                            break;
+                        switch (endpointEvent.Event.EnumType)
+                        {
+                            case EventType.Get:
+                                response = RestfulUtility.Get<object>(url, endpointEvent.JWTToken, new List<Header> { });
+                                break;
+                            case EventType.Post:
+                                response = RestfulUtility.Post<object, object>(url, endpointEvent.JWTToken, new Package<dynamic>
+                                {
+                                    Headers = new List<Header> { },
+                                    Body = body
+                                });
+                                break;
+                            case EventType.Put:
+                                response = RestfulUtility.Put<object, object>(url, endpointEvent.JWTToken, new Package<dynamic>
+                                {
+                                    Headers = new List<Header> { },
+                                    Body = body
+                                });
+                                break;
+                            case EventType.Patch:
+                                response = RestfulUtility.Patch<object, object>(url, endpointEvent.JWTToken, new Package<dynamic>
+                                {
+                                    Headers = new List<Header> { },
+                                    Body = body
+                                },
+                                "application/json-patch+json");
+                                break;
+                            case EventType.Delete:
+                                response = RestfulUtility.Delete<object>(url, endpointEvent.JWTToken, new List<Header> { });
+                                break;
+                            default:
+                                response = new Object { };
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        switch (endpointEvent.Event.EnumType)
+                        {
+                            case EventType.Get:
+                                response = RestfulUtility.Get<object>(url, new List<Header> { });
+                                break;
+                            case EventType.Post:
+                                response = RestfulUtility.Post<object, object>(url, new Package<dynamic>
+                                {
+                                    Headers = new List<Header> { },
+                                    Body = body
+                                });
+                                break;
+                            case EventType.Put:
+                                response = RestfulUtility.Put<object, object>(url, new Package<dynamic>
+                                {
+                                    Headers = new List<Header> { },
+                                    Body = body
+                                });
+                                break;
+                            case EventType.Patch:
+                                response = RestfulUtility.Patch<object, object>(url, new Package<dynamic>
+                                {
+                                    Headers = new List<Header> { },
+                                    Body = body
+                                },
+                                "application/json-patch+json");
+                                break;
+                            case EventType.Delete:
+                                response = RestfulUtility.Delete<object>(url, new List<Header> { });
+                                break;
+                            default:
+                                response = new Object { };
+                                break;
+                        }
                     }
                     string responseString = JsonUtility.Serialize(response);
                     this.Logger.LogInformation(responseString);
